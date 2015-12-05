4512be7e252f8016e729485a30ff91dccf8497cc
Ivan Zhur <vzhur98@gmail.com>
2015-12-05 18:30:37 +0200

    Save default graphs in MainActivity. Remove AddActivity.

--- app/src/main/java/com/ivanzhur/shakeunlock/MainActivity.java
+++ app/src/main/java/com/ivanzhur/shakeunlock/MainActivity.java
@@ -18,6 +18,7 @@ import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.GridLabelRenderer;
@@ -39,7 +40,8 @@ public class MainActivity extends Activity implements SensorEventListener {
     final int maxPointsOnGraph = 100;
     int savingDefault = -1;
     long lastUpdateTime = 0;
-    boolean watchingAccData = true;
+    boolean watchingAccData, defaultStarted, defaultButtonStart;
+    int[] graphNumber;
 
     TextView statusTextView, instructionsTextView;
     Button changeButton, startStopButton;
@@ -67,11 +69,14 @@ public class MainActivity extends Activity implements SensorEventListener {
         setUI();
 
         series = new LineGraphSeries<>();
-        series.setColor(Color.BLACK);
+        series.setColor(Color.BLUE);
 
         graphView.removeAllSeries();
         graphView.addSeries(series);
         graphPoints = new ArrayList<>();
+
+        watchingAccData = true;
+        graphNumber = new int[]{R.string.pattern_1, R.string.pattern_2, R.string.pattern_3};
     }
 
     public void setUI(){
@@ -94,6 +99,10 @@ public class MainActivity extends Activity implements SensorEventListener {
         graphView.getGridLabelRenderer().setVerticalLabelsVisible(false);
         graphView.getGridLabelRenderer().setPadding(0);
 
+        setTextAndButtons();
+    }
+
+    private void setTextAndButtons(){
         if (preferences.getBoolean(SERVICE_ACTIVE, false)){
             statusTextView.setText(R.string.service_running);
             startStopButton.setText(R.string.service_stop);
@@ -141,6 +150,14 @@ public class MainActivity extends Activity implements SensorEventListener {
         double sum = Math.sqrt(x*x+y*y+z*z);
         sum = Math.round(sum*100)/100.0;
 
+        // Start measuring default values
+        if (savingDefault > 0 && !defaultStarted && Math.abs(sum-Graph.GRAVITY) < Graph.START_THRESHOLD) return;
+        if (!defaultStarted){
+            defaultStarted = true;
+            instructionsTextView.setVisibility(View.GONE);
+            graphView.setVisibility(View.VISIBLE);
+        }
+
         graphPoints.add(new GraphPoint(sum, x, y, z));
 
         int size = graphPoints.size();
@@ -166,67 +183,123 @@ public class MainActivity extends Activity implements SensorEventListener {
     }
 
     public void onChangeButtonClick(View view){
-        Intent intent = new Intent(getApplicationContext(), AddActivity.class);
-        //startActivity(intent);
-
+        // Initial state. Show instructions.
         if (savingDefault == -1){
             graphView.setVisibility(View.GONE);
-            statusTextView.setVisibility(View.GONE);
+            statusTextView.setVisibility(View.INVISIBLE);
             instructionsTextView.setVisibility(View.VISIBLE);
+            instructionsTextView.setGravity(Gravity.LEFT);
+            instructionsTextView.setText(R.string.instructions_text);
             changeButton.setText(R.string.pattern_cancel);
             startStopButton.setText(R.string.pattern_start);
             changeButton.setTextColor(ContextCompat.getColor(this, R.color.red));
             startStopButton.setTextColor(ContextCompat.getColor(this, R.color.green));
             savingDefault = 0;
             watchingAccData = false;
+            defaultStarted = false;
+            defaultButtonStart = true;
         }
-        else if (savingDefault == 0){
+        // User cancelled saving defaults. Return interface to initial state.
+        else if (savingDefault == 0 || savingDefault > 3){
             graphView.setVisibility(View.VISIBLE);
             statusTextView.setVisibility(View.VISIBLE);
             instructionsTextView.setVisibility(View.GONE);
             changeButton.setText(R.string.pattern_change);
             changeButton.setTextColor(ContextCompat.getColor(this, R.color.black));
-            boolean serviceActive = preferences.getBoolean(SERVICE_ACTIVE, false);
-            startStopButton.setText((serviceActive) ? R.string.service_stop : R.string.service_start);
-            startStopButton.setTextColor(ContextCompat.getColor(this, (serviceActive) ? R.color.red : R.color.green));
             savingDefault = -1;
             watchingAccData = true;
+            setTextAndButtons();
         }
     }
 
     public void onStartStopClick(View view){
+        // Saving defaults
         if (savingDefault > -1){
-            if (savingDefault == 0){
+            // Start measurement. Show next instruction, change buttons, clear graphPoints if first time.
+            if (defaultButtonStart){
+                statusTextView.setVisibility(View.VISIBLE);
+                graphView.setVisibility(View.INVISIBLE);
+                instructionsTextView.setVisibility(View.VISIBLE);
+                changeButton.setVisibility(View.INVISIBLE);
+
+                statusTextView.setText(graphNumber[savingDefault]);
+                statusTextView.setTextColor(ContextCompat.getColor(this, R.color.black));
                 instructionsTextView.setText(R.string.instructions_start);
                 instructionsTextView.setGravity(Gravity.CENTER);
-                defaults = new ArrayList<>();
+                startStopButton.setText(R.string.pattern_stop);
+                startStopButton.setTextColor(ContextCompat.getColor(this, R.color.black));
+
+                if (savingDefault == 0) defaults = new ArrayList<>();
                 graphPoints.clear();
-                savingDefault = 1;
+                savingDefault++;
+                watchingAccData = true;
+                defaultStarted = false;
+                defaultButtonStart = false;
+            }
+            else { // Stop measurement
+                if (!defaultStarted) return; // Graph not measured
+
+                watchingAccData = false;
+                if (savingDefault <= 3) defaults.add(new Graph(graphPoints)); // Add measured graph
+
+                if (savingDefault < 3){ // NOT all 3 graphs are measured
+                    startStopButton.setText(R.string.pattern_next);
+                    defaultButtonStart = true;
+                }
+                else if (savingDefault < 4){ // All 3 graphs are just measured. Compare them.
+                    boolean sim_12 = Graph.compareGraphs(defaults.get(0), defaults.get(1));
+                    boolean sim_13 = Graph.compareGraphs(defaults.get(0), defaults.get(2));
+                    boolean sim_23 = Graph.compareGraphs(defaults.get(1), defaults.get(2));
+                    boolean similar = sim_12 && sim_13 && sim_23;
+
+                    statusTextView.setVisibility(View.VISIBLE);
+                    statusTextView.setText((similar) ? R.string.pattern_similar : R.string.pattern_not_similar);
+                    statusTextView.setTextColor(ContextCompat.getColor(this, (similar) ? R.color.green : R.color.red));
+
+                    changeButton.setVisibility(View.VISIBLE);
+                    changeButton.setText((similar) ? R.string.pattern_dont_save : R.string.pattern_cancel);
+
+                    startStopButton.setText((similar) ? R.string.pattern_save : R.string.pattern_retry);
+                    startStopButton.setTextColor(ContextCompat.getColor(this, (similar) ? R.color.green : R.color.black));
+                    savingDefault = (similar) ? 4 : 5;
+                }
+                else if (savingDefault == 4){ // 'Save' pressed. Save defaults and return interface to initial state.
+                    saveDefaults();
+                    setTextAndButtons();
+                    changeButton.setText(R.string.pattern_change);
+                    changeButton.setTextColor(ContextCompat.getColor(this, R.color.black));
+                    graphView.setVisibility(View.VISIBLE);
+                    watchingAccData = true;
+                    savingDefault = -1;
+                    Toast.makeText(this, R.string.pattern_saved, Toast.LENGTH_SHORT).show();
+                }
+                else if (savingDefault == 5){ // 'Retry' pressed. Start saving again.
+                    savingDefault = 0;
+                    defaultButtonStart = true;
+                    onStartStopClick(view);
+                }
             }
 
             return;
         }
 
-
-        Log.i(TAG, "act: " + preferences.getBoolean(SERVICE_ACTIVE, false));
+        // Not saving defaults. Switching service on and off.
         if (preferences.getBoolean(SERVICE_ACTIVE, false)){
-            statusTextView.setText(R.string.service_stopped);
-            startStopButton.setText(R.string.service_start);
-            statusTextView.setTextColor(ContextCompat.getColor(this, R.color.red));
-            startStopButton.setTextColor(ContextCompat.getColor(this, R.color.green));
             editor.putBoolean(SERVICE_ACTIVE, false);
-
             stopService(accelerometerService);
         }
         else {
-            statusTextView.setText(R.string.service_running);
-            startStopButton.setText(R.string.service_stop);
-            statusTextView.setTextColor(ContextCompat.getColor(this, R.color.green));
-            startStopButton.setTextColor(ContextCompat.getColor(this, R.color.red));
             editor.putBoolean(SERVICE_ACTIVE, true);
-
             startService(accelerometerService);
         }
         editor.apply();
+        setTextAndButtons();
+    }
+
+    private void saveDefaults(){
+        for (int i=0; i<3; i++) {
+            MainActivity.editor.putString(MainActivity.DEFAULT[i], Graph.getJson(defaults.get(i)));
+            MainActivity.editor.apply();
+        }
     }
 }
